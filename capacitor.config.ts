6dbebdcd402996f2a17b11a6541cf4741222796b
Edwin Okoronkwo <49468467+EdwinOkoronkwo@users.noreply.github.com>
2024-02-03 05:25:24 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'greasy_bears_app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
